b1001382bb50aad057e17bd00c081841562ac734
Ravula Pranay Kumar Reddy <rpranayreddy40@gmail.com>
2024-02-04 20:13:02 +0530

    Plan for Audit done

--- src/main/java/com/nextuple/pranay/backend/controller/output/Audit.java
+++ src/main/java/com/nextuple/pranay/backend/controller/output/Audit.java
@@ -1,5 +1,132 @@
 package com.nextuple.pranay.backend.controller.output;
 
-public class Audit {
+import java.util.List;
 
+public class Audit {
+    private Sales sales;
+    private Purchase purchase;
+    private Profit profit;
+    private ProductWithoutInventory productWithoutInventory;
+    private ProductWithStockLessThanSafeQuantity productWithStockLessThanSafeQuantity;
+    /*
+    Sales
+        -> orderCount
+        -> uniqueProductCount
+        -> totalItemCount
+        -> totalIncome
+        -> productDetails (sort by itemCount)
+            -> productId
+            -> productName
+            -> itemCount
+            -> totalIncome
+            -> incomePerItem
+     */
+    public static class Sales{
+        private long orderCount;
+        private long uniqueProductCount;
+        private long totalItemCount;
+        private double totalIncome;
+        private List<ProductSales> productDetails;
+        public static class ProductSales{
+            private String productId;
+            private String productName;
+            private long itemCount;
+            private double totalIncome;
+            private double incomePerItem;
+        }
+
+    }
+    /*
+    Purchase
+        -> orderCount
+        -> uniqueProductCount
+        -> totalItemCount
+        -> totalExpenditure
+        -> productDetails (sort by itemCount)
+            -> productId
+            -> productName
+            -> itemCount
+            -> totalExpenditure
+            -> expenditurePerItem
+     */
+    public static class Purchase{
+        private long orderCount;
+        private long uniqueProductCount;
+        private long totalItemCount;
+        private double totalExpenditure;
+        private List<ProductPurchase> productDetails;
+        public static class ProductPurchase{
+            private String productId;
+            private String productName;
+            private long itemCount;
+            private double totalExpenditure;
+            private double expenditurePerItem;
+        }
+    }
+    /*
+    Profit
+        -> totalProfit
+        -> productDetails (sort by profit)
+            -> productId
+            -> productName
+            -> totalProfit
+            -> profitPerItem
+            -> profitRank
+     */
+    public static class Profit {
+        private double totalProfit;
+        private List<ProductProfit> productDetails;
+
+        public static class ProductProfit {
+            private String productId;
+            private String productName;
+            private double totalProfit;
+            private double profitPerItem;
+            private long profitRank;
+        }
+    }
+    /*
+    Product without inventory
+        -> ProductDetails
+            -> productId
+            -> productName
+            -> productCategory
+     */
+    public static class ProductWithoutInventory {
+        private List<ProductDetails> productDetails;
+
+        public static class ProductDetails {
+            private String productId;
+            private String productName;
+            private String productCategory;
+        }
+    }
+    /*
+    Products with stock less than safe quantity
+        -> countOfProduct
+        -> totalExpenditureNeeded
+        -> ProductDetails
+            -> productId
+            -> productName
+            -> currentStock
+            -> safeQuantity
+            -> stockNeeded
+            -> expenditureNeeded
+            -> profitRank
+     */
+    public static class ProductWithStockLessThanSafeQuantity {
+        private long countOfProduct;
+        private double totalExpenditureNeeded;
+        private List<ProductDetails> productDetails;
+
+        public static class ProductDetails {
+            private String productId;
+            private String productName;
+            private int currentStock;
+            private int safeQuantity;
+            private int stockNeeded;
+            private double expenditureNeeded;
+            private long profitRank;
+        }
+    }
 }
--- src/main/java/com/nextuple/pranay/backend/service/AuditServices.java
+++ src/main/java/com/nextuple/pranay/backend/service/AuditServices.java
@@ -2,6 +2,8 @@ package com.nextuple.pranay.backend.service;
 
 import com.nextuple.pranay.backend.controller.output.Audit;
 import com.nextuple.pranay.backend.model.Inventory;
+import com.nextuple.pranay.backend.model.Order;
+import com.nextuple.pranay.backend.model.Product;
 import com.nextuple.pranay.backend.repo.OrderRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +12,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AuditServices {
 
     @Autowired
-    OrderRepo orderRepo;
+    OrderServices orderServices;
     @Autowired
     ProductServices productServices;
     @Autowired
@@ -22,13 +26,17 @@ public class AuditServices {
     private static final Logger logger = LoggerFactory.getLogger(OrderServices.class);
 
     public ResponseEntity<?> audit() {
-        //TODO: Total sales
-        //TODO: Total purchase
-        //TODO: Total profit (only caluclate if purchase && sales are available)
         //TODO: Product sorted by sales
         //TODO: Product sorted by purchase
         //TODO: Product sorted by profit
+        //TODO: Product without inventory
         //TODO: Product with stock less than safe quantity
+        List<Inventory> inventories = inventoryServices.listAllInventory().getBody();
+        List<Order> orders = orderServices.listAllOrders().getBody();
+        List<Product> products = productServices.listAllProducts().getBody();
+
+
+
         return new ResponseEntity<>("send audit data", HttpStatus.OK);
     }
 }
